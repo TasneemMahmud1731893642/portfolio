165c7bda79193d84b76052321ef531df944e983c
tazz111233 <tasneem@codingincolour.org>
2025-03-26 13:11:01 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import { FiSmartphone } from 'react-icons/fi';
+//import { FiSmartphone } from 'react-icons/fi';
 import bharatiyaImage from './img/bharatiya.png';
 import me from './img/me.png';
 import jt from './img/jt.png';
-import monitor from './img/monitor.png';
+//import monitor from './img/monitor.png';
 import notes from './img/notes.png';
 import learning from './img/learning.png';
 import movie from './img/movie.png';
 import linkedin from './img/linkedin.png';
-import { FiMonitor } from 'react-icons/fi'; 
+//import { FiMonitor } from 'react-icons/fi'; 
 
 function App() {
   return (
@@ -40,7 +40,7 @@ function App() {
 
       </div>
       {/* <div className="im"> */}
-      <img className="monitor1" src={bharatiyaImage}/>
+      <img className="monitor1" src={bharatiyaImage} alt="IM"/>
        {/* <div className="box"></div> */}
 
 
@@ -56,7 +56,7 @@ function App() {
           <p>This full-stack eCommerce website, built for PlinkBd, delivers a seamless shopping experience with a modern and intuitive UI. It includes all essential eCommerce features, such as a product catalog, shopping cart, discount code integration, and a secure checkout process. The platform also features a robust admin panel, enabling efficient order management, product stock control, and discount code management. Developed with React.js for the frontend and Node.js/Express.js for the backend, the application ensures high performance, scalability, and smooth user interaction.</p>
 
       </div>
-      <img className="monitor1" src={jt}/>
+      <img className="monitor1" src={jt} alt="IM"/>
       
       
 
@@ -68,7 +68,7 @@ function App() {
           <p>An innovative application designed to help multiple users efficiently store and manage their notes in one unified platform. Each note includes key details such as the title, author’s name, and the content itself, making organization effortless. In addition to shared notes, the application also provides users with a secure space to store personal notes privately. With powerful features like a search function and the ability to sort notes based on various parameters, users can easily find and manage their content. Built using ReactJS, the application is not only comprehensive in its functionality but also concise, ensuring smooth performance and a user-friendly experience.</p>
 
       </div>
-      <img className="monitor1" src={notes}/>
+      <img className="monitor1" src={notes} alt="IM"/>
       
       
 
@@ -80,7 +80,7 @@ function App() {
           <p>A platform crafted to revolutionize learning by offering all the essential resources needed to master a wide range of topics. Tailored for modern learners, it combines traditional educational tools with the latest advancements in technology. In this age of artificial intelligence, the platform stands out with a seamlessly integrated AI chatbot powered by OpenAI's ChatGPT. This powerful feature provides instant access to personalized assistance, in-depth explanations, and a wealth of information, making learning more interactive and engaging. With its unique ability to deliver unlimited knowledge and resources in one centralized location, this platform is truly a one-of-a-kind solution for learners of all ages and backgrounds.</p>
 
       </div>
-      <img className="monitor2" src={learning}/>
+      <img className="monitor2" src={learning} alt="IM"/>
       
       
 
@@ -92,7 +92,7 @@ function App() {
           <p>An indispensable platform for every movie theater: an advanced online ticketing and user management system. This platform enables users to explore movie options, watch trailers, and conveniently purchase tickets for their favorite films. With a robust SQL database, it ensures efficient storage and management of user and ticketing data. The backend, powered by PHP, handles all server-side operations, including secure transactions and user authentication, providing a smooth and reliable experience. This system streamlines theater operations while enhancing the user experience, making it a must-have for modern cinemas.</p>
 
       </div>
-      <img className="monitor3" src={movie}/>
+      <img className="monitor3" src={movie} alt="IM"/>
       
       
 
